8379f8b0ebbdc41156271d71988532d6d679400f
Devin Schwab <digidevin@gmail.com>
2017-01-30 14:17:37 -0500

    Fixed bug

--- rl/callbacks.py
+++ rl/callbacks.py
@@ -375,5 +375,5 @@ class TensorboardCallback(Callback):
                 self.writer.add_summary(summary, self.log_step)
             self.writer.flush()
 
-        if self.is_training:
+        if self.model.is_training:
             self.log_step += 1
